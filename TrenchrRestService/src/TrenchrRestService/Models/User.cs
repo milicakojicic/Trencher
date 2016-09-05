cfc54f4917fa9fbfd5c4d1f84aa13be68ce52117
IgorRodic <igorrodic@gmail.com>
2016-09-05 21:44:08 +0200

    Implementiranje funkcije za vracanje svih konverzacija, takodje dopunjeni i potrebni modeli.

--- TrenchrRestService/src/TrenchrRestService/Models/User.cs
+++ TrenchrRestService/src/TrenchrRestService/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Neo4j.Driver.V1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,5 +13,14 @@ namespace TrenchrRestService.Models
         public string Surname { get; set; }
         public string Email { get; set; }
         public string PicturePath { get; set; }
+
+        public User(IRecord record)
+        {
+            ID = (long)record["id"];
+            Name = (string)record["name"];
+            Surname = (string)record["surname"];
+            Email = (string)record["email"];
+            PicturePath = (string)record["picture_path"];
+        }
     }
 }
