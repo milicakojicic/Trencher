857d3c34c988786832dad456cf6c309a43a371c0
nsub93 <suboticnemanja93@gmail.com>
2016-09-28 01:02:28 +0200

    uradjen users - identity

--- TrenchrRestService/src/IdentityService/Configuration/Users.cs
+++ TrenchrRestService/src/IdentityService/Configuration/Users.cs
@@ -11,32 +11,37 @@ namespace IdentityService.Configuration
     {
         public static List<InMemoryUser> Get()
         {
-            var users = new List<InMemoryUser>
-            {
-                new InMemoryUser{Subject ="1", Username = "alice", Password = "alice",
-                    Claims = new[]
-                    {
-                        new Claim(Constants.ClaimTypes.Name, "Alice Smith"),
-                        new Claim(Constants.ClaimTypes.GivenName, "Alice"),
-                        new Claim(Constants.ClaimTypes.FamilyName, "Smith"),
-                        new Claim(Constants.ClaimTypes.Email, "AliceSmith@email.com"),
-                        new Claim(Constants.ClaimTypes.Role, "Admin"),
-                        new Claim(Constants.ClaimTypes.Role, "Geek"),
-                    }
-                },
-                new InMemoryUser{Subject = "2", Username = "bob", Password = "bob",
-                    Claims = new[]
-                    {
-                        new Claim(Constants.ClaimTypes.Name, "Bob Smith"),
-                        new Claim(Constants.ClaimTypes.GivenName, "Bob"),
-                        new Claim(Constants.ClaimTypes.FamilyName, "Smith"),
-                        new Claim(Constants.ClaimTypes.Email, "BobSmith@email.com"),
-                        new Claim(Constants.ClaimTypes.Role, "Developer"),
-                        new Claim(Constants.ClaimTypes.Role, "Geek"),
-                    }
-                }
-            };
+            var users = new List<InMemoryUser>();
+            var stmnt = "match (s:student) return id(s) as subject, s.name as name, s.ime as given_name, s.prezime as family_name, " +
+                                                " s.email as email, s._password as password , s._claimRoles as roles";
 
+            var result = Neo4jClient.Execute(stmnt);
+            foreach (var row in result)
+            {
+                var user = new InMemoryUser
+                {
+                    Subject = (string)row["subject"],
+                    Username = (string)row["email"],
+                    Password = (string)row["password"]
+                };
+
+                var claims = new List<Claim>
+                {
+                    new Claim(Constants.ClaimTypes.Name, (string)row["name"]),
+                    new Claim(Constants.ClaimTypes.GivenName, (string)row["given_name"]),
+                    new Claim(Constants.ClaimTypes.FamilyName,(string)row["family_name"] ),
+                    new Claim(Constants.ClaimTypes.Email, (string)row["email"])
+                };
+
+
+                var roles = (string[])row["roles"];
+                foreach (string role in roles)
+                    claims.Add(new Claim(Constants.ClaimTypes.Role, role));
+
+                user.Claims = claims;
+                users.Add(user);
+
+            }
             return users;
         }
     }
